9c213a7363156fd0ba6c035de98d5d3bb73d4967
Pejman Pourshirazi <pspejman@gmail.com>
2026-08-22 10:02:37 +0330

    Add Sergin.UserAccess.Application.Contracts project shell

---
+++ Sergin.UserAccess.Application.Contracts/UserAccessApplicationContractsAssemblyReference.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+namespace Sergin.UserAccess.Application.Contracts;
+
+public static class UserAccessApplicationContractsAssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(UserAccessApplicationContractsAssemblyReference).Assembly;
+}
